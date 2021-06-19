74d2b986fcd4e6b4af2ad3fedc3bb466d395cede
CJdebota <80597304+CJdebota@users.noreply.github.com>
2021-06-19 09:48:41 -0300

    Add files via upload

---
+++ Aula 10/config.js
@@ -0,0 +1,16 @@
+const s11 = document.querySelector('header');
+s11.insertAdjacentHTML("afterbegin",'<h1> Aula 10 </h1>');
+s11.insertAdjacentHTML("beforeend",'<p> Arthur Rafael <p>');
+
+const s12 = document.querySelector('main'),
+main = document.createElement('div');
+document.body.main.div.insertAdjacentHTML("afterbegin",'<h2> Programação web </h2>');
+document.body.main.div.insertAdjacentHTML("afterbegin",'<p> Sus </p>');
+
+const s13 = document.querySelector('main');
+main = document.createElement('div');
+div = document.appendChild(main);
+document.body.main.div.insertAdjacentHTML("afterbegin",'<h2> Error </h2>');
+document.body.main.div.insertAdjacentHTML("afterbegin",'<p> Unexpecteded </p>');
+
+
