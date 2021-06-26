3d663f6e72711caca5d0b2e86d93d448577719d7
CJdebota <80597304+CJdebota@users.noreply.github.com>
2021-06-26 09:00:52 -0300

    Add files via upload

--- Aula 10/config.js
+++ Aula 10/config.js
@@ -1,16 +1,37 @@
-const s11 = document.querySelector('header');
-s11.insertAdjacentHTML("afterbegin",'<h1> Aula 10 </h1>');
-s11.insertAdjacentHTML("beforeend",'<p> Arthur Rafael <p>');
+const w129 = document.querySelector("#a1");
+w129.id = 'a1b';
+w129.insertAdjacentHTML('afterbegin', "<h1>Enxurrada de Bits</h1>");
+    
 
-const s12 = document.querySelector('main'),
-main = document.createElement('div');
-document.body.main.div.insertAdjacentHTML("afterbegin",'<h2> Programação web </h2>');
-document.body.main.div.insertAdjacentHTML("afterbegin",'<p> Sus </p>');
+const x = document.querySelector("#a2");
 
-const s13 = document.querySelector('main');
-main = document.createElement('div');
-div = document.appendChild(main);
-document.body.main.div.insertAdjacentHTML("afterbegin",'<h2> Error </h2>');
-document.body.main.div.insertAdjacentHTML("afterbegin",'<p> Unexpecteded </p>');
+let x129 = document.createElement('div');
+let section = document.createElement('section');
+
+    let x189 = document.createElement('h2');
+    let x189b = document.createTextNode('Enxurrada de bits')
+
+    let x192 = document.createElement('p');
+    let x192b = document.createTextNode('Aula 10');
+
+    x189.appendChild(x189b);
+    x129.appendChild(x189);
+
+    x192.appendChild(x192b);
+    x129.appendChild(x192);
+
+    let x001 = document.createElement('h2');
+    let x001b = document.createTextNode('Programação-web')
+
+    let x002 = document.createElement('p');
+    let x002b = document.createTextNode('Arthur Rafael');
+
+    x001.appendChild(x001b);
+    x129.appendChild(x001);
+
+    x002.appendChild(x002b);
+    x129.appendChild(x002);
 
 
+
+document.getElementById('a2').insertAdjacentElement('afterbegin',x129);
